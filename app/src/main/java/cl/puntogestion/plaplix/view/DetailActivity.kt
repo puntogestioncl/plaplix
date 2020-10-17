fdc3efaa02cf796ccf2f59f803df93befd6ef6b8
puntogestioncl <git@puntogestion.cl>
2020-10-17 17:01:57 -0300

    Segundo commit

--- app/src/main/java/cl/puntogestion/plaplix/view/DetailActivity.kt
+++ app/src/main/java/cl/puntogestion/plaplix/view/DetailActivity.kt
@@ -1,5 +1,8 @@
 package cl.puntogestion.plaplix.view
 
+import android.content.Intent
+import android.net.MailTo
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.google.android.material.appbar.CollapsingToolbarLayout
@@ -10,6 +13,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import cl.puntogestion.plaplix.R
 import kotlinx.android.synthetic.main.activity_detail.*
+import javax.security.auth.Subject
 
 class DetailActivity : AppCompatActivity() {
 
@@ -21,13 +25,24 @@ class DetailActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-
+            composeEmail(Array(2, ["sdfsd", "dffsd"]), "prueba")
         }
     }
 
     override fun onStart() {
         super.onStart()
 
+    }
+
+    fun composeEmail(addresses: Array<String>, subject: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:") // only email apps should handle this
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 
 }
